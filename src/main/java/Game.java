b0ff423ed0bc168c78d0d72fd4a22353916a404f
joaooliveiraa11 <93929858+joaooliveiraa11@users.noreply.github.com>
2022-10-05 22:22:22 +0100

    7. Reading keystrokes, complete

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -12,32 +12,59 @@ public class Game {
     private Screen screen;
 
     public Game() throws IOException {
-            TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            Terminal terminal = terminalFactory.createTerminal();
-            
-            screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
+        TerminalSize terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
     }
 
-        private void draw () throws IOException {
-            screen.clear();
-            screen.setCharacter(x,y, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
-        }
-        public void run () throws IOException {
+    private void draw() throws IOException {
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+    public void run() throws IOException {
+        boolean test = true;
+        while (test) {
             draw();
             KeyStroke key = screen.readInput();
             processKey(key);
+            switch (key.getKeyType()) {
+                case ArrowUp:
+                    y -= 1;
+                    break;
+                case ArrowDown:
+                    y += 1;
+                    break;
+                case ArrowRight:
+                    x += 1;
+                    break;
+                case ArrowLeft:
+                    x -= 1;
+                    break;
+                case Character:
+                    if (key.getCharacter() == 'q') {
+                        screen.close();
+                    }
+                    break;
+                case EOF:
+                    test = false;
+                    break;
+            }
         }
-
-        private int x = 10;
-        private int y = 10;
-
-        private void processKey(KeyStroke key) {
-            System.out.println(key);
-        }
-
     }
+
+    private int x = 10;
+    private int y = 10;
+
+    private void processKey(KeyStroke key) {
+        System.out.println(key);
+    }
+
+
+}
